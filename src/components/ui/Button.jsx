fd3112845c4092907e5f5dd05f3d790ab2a62eec
elviredev <sandrine.rodriguez@hotmail.com>
2026-06-30 18:59:38 +0200

    🍷 Button, TextInput

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import { Loader2 } from "lucide-react"
 
-const Button = () => {
+/**
+ * @param {{
+ *   children: React.ReactNode,
+ *   type?: "button" | "submit" | "reset",
+ *   icon?: React.ComponentType<{ className?: string }>,
+ *   className?: string,
+ *   loading?: boolean,
+ *   loadingText?: string,
+ *   disabled?: boolean,
+ *   onClick?: React.MouseEventHandler<HTMLButtonElement>
+ * }} props
+ */
+const Button = ({
+  children,
+  type = "button",
+  icon: Icon,
+  loading = false,
+  loadingText,
+  disabled = false,
+  className = "",
+  ...props
+}) => {
   return (
-    <div>Button</div>
+    <button
+      type={type}
+      disabled={disabled || loading}
+      className={`w-full md:w-auto bg-linear-to-r from-rose-600 to-red-800 hover:from-rose-700 hover:to-red-900 text-white rounded-xl px-6 py-3 text-sm font-semibold transition duration-200 shadow-md shadow-rose-200 flex items-center justify-center gap-2 whitespace-nowrap disabled:opacity-50 disabled:cursor-not-allowed ${className}`}
+      {...props}
+    >
+      
+      {loading ? (
+        <Loader2 className="w-4 h-4 animate-spin" />
+      ) : (
+        Icon && <Icon className="w-4 h-4" />
+      )}
+
+      {loading ? loadingText || children : children}
+    </button>
   )
 }
 
--- src/components/ui/TextInput.jsx
+++ src/components/ui/TextInput.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
-
-const TextInput = () => {
+const TextInput = ({
+  label,
+  name,
+  type = "text",
+  required = false,
+  error,
+  className = "",
+  ...props
+}) => {
   return (
-    <div>TextInput</div>
+    <div className='mb-4'>
+      {label && (
+        <label
+          htmlFor={name}
+          className='block text-sm font-medium text-gray-700'
+        >
+          {label} 
+          {required && <span className="text-red-500">*</span>}
+        </label>
+      )}
+
+      <input
+        id={name}
+        name={name}
+        type={type}        
+        className={`w-full px-4 py-3 text-gray-700 bg-white rounded-lg border border-gray-200 placeholder-gray-500 transition-all focus:outline-none focus:ring-0 focus:border-rose-700 focus:shadow-none
+          ${error ? "border-red-500" : "border-gray-200"} ${className}`}
+        {...props}
+      />
+
+      {error && (
+        <p className="mt-1 text-sm text-red-600">
+          {error}
+        </p>
+      )}
+
+    </div>
   )
 }
 
